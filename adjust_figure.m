650bfca7b9f287577f2e2a876b8a1de24dd64e87
Charles Zhu <zhuqiuxiuci@gmail.com>
2015-12-16 20:14:09 -0800

    Improved figures

---
+++ adjust_figure.m
@@ -0,0 +1,7 @@
+set(gca, 'FontSize', 14);
+set(findall(gca, 'Type', 'Line'), 'LineWidth', 2);
+set(findall(gca, 'Type', 'Line'), 'MarkerSize', 10);
+%legend('Location', 'southeast');
+%legend('Location', 'northwest');
+%legend('Strict static plan', 'Strict timeline', 'Adaptive grace period');
+%xlabel('Average size of data chunks (KB)');
