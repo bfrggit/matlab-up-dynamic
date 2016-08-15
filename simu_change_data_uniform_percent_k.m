af64762b9dfa0fecc09423480344401e95fa3743
Charles Zhu <zhuqiuxiuci@gmail.com>
2016-08-14 19:44:07 -0700

    Added scritps for ds_size_dy_data

--- simu_change_data_uniform_percent_k.m
+++ simu_change_data_uniform_percent_k.m
@@ -1,10 +1,20 @@
 function [changed_data] = simu_change_data_uniform_percent_k(v_ds_in, ...
     half_range_percent)
 changed_data = v_ds_in;
-ds_sizes = changed_data(:, 3);
 changed_data(:, 3) = changed_data(:, 3).* ( ...
         rand(size(changed_data(:, 3))) * half_range_percent * 2 ...
       - half_range_percent);
+
+n_ds = size(changed_data, 1);
+for j = 1:n_ds
+    if rand() < half_range_percent
+        if changed_data(j, 5) < 0.5
+            changed_data(j, 5) = 0.6;
+        elseif changed_data(j, 5) < 0.8
+            changed_data(j, 5) = 1.0;
+        end
+    end
+end
 
 end
 
